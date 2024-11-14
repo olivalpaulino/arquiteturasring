b337e799b7242287365104b63ce9e92d91a1424d
Olival Paulino <olivalpaulinocc@gmail.com>
2024-11-14 07:07:14 -0300

    primeiro commit

---
+++ src/main/java/br/com/dobackaofront/arquiteturasring/ArquiteturasringApplication.java
@@ -0,0 +1,13 @@
+package br.com.dobackaofront.arquiteturasring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturasringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturasringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dobackaofront/arquiteturasring/ArquiteturasringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dobackaofront.arquiteturasring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturasringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
